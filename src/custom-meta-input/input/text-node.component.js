fe68b98aee9865188c103385aeea7898bb128637
Frankie Yan <frankieyan@gmail.com>
2019-10-03 21:44:36 +0800

    Extract pill and text identifiers into constants

--- src/custom-meta-input/input/text-node.component.js
+++ src/custom-meta-input/input/text-node.component.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import styled from 'styled-components'
+import { TEXT_NODE_IDENTIFIER } from './input.constants'
 
-const TextNode = styled.span`
+const TextNode = styled(forwardRef(({ children, ...rest }, ref) => (
+  <span {...{ [TEXT_NODE_IDENTIFIER]: true }} ref={ref} {...rest}>
+    {children}
+  </span>
+)))`
   white-space: pre;
 `
 

2725a75dc50d824655d54f4dc6fcfc55f7743913
Frankie Yan <frankieyan@gmail.com>
2019-10-07 18:25:12 +0800

    Add spec around text selection

--- src/custom-meta-input/input/input.spec.js
+++ src/custom-meta-input/input/input.spec.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { render, fireEvent, wait } from 'react-testing-library'
+import { render, fireEvent } from 'react-testing-library'
 import 'jest-dom/extend-expect'
 
 import { Input } from './input.component'
 
 describe('<Input> component', () => {
+  beforeEach(() => {
+    window.getSelection = jest.fn()
+  })
+
   test('renders a focusable element', () => {
     const { container } = render(<Input />)
 
@@ -86,6 +90,41 @@ describe('<Input> component', () => {
 
       fireEvent.keyDown(container.firstChild, { key: ' ' })
       expect(handler).lastCalledWith('I\'m feeling good ')
+    })
+  })
+
+  describe('when content is selected', () => {
+    test('pills are rendered differently', () => {
+      const { getByText } = render(<Input value="I'm listening to {{Song Name__Space Oddity}} right now" />)
+      window.getSelection.mockReturnValue({
+        isCollapsed: false,
+        anchorNode: getByText('right now'),
+        focusNode: getByText('I\'m listening to'),
+        anchorOffset: 10,
+        focusOffset: 0,
+      })
+
+      fireEvent.mouseMove(getByText('Space Oddity'))
+
+      expect(getByText('Song Name').parentElement).toHaveStyle('background-color: lightgreen')
+    })
+
+    test('the delete key removes the selected elements', () => {
+      const handler = jest.fn()
+      const { getByText, container } = render(<Input value="I'm listening to {{Song Name__Space Oddity}} right now, and I give it a {{Rating__5}}" onChange={handler} />)
+      window.getSelection.mockReturnValue({
+        isCollapsed: false,
+        anchorNode: getByText('Rating'),
+        focusNode: getByText('I\'m listening to'),
+        anchorOffset: 5,
+        focusOffset: 13,
+        removeAllRanges: jest.fn(),
+      })
+
+      fireEvent.mouseMove(getByText('Space Oddity'))
+      fireEvent.keyDown(container.firstChild, { key: 'Backspace' })
+
+      expect(handler).lastCalledWith('I\'m listening')
     })
   })
 })
